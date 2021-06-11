7982fc20f60b11c60a1b1556b8e68ab768ebdcd7
vincent <vincentvanderstruis@icloud.com>
2021-06-11 16:56:55 +0200

    Updates

---
+++ precache-manifest.a6322a980344eb286f3e262cd597b9e0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "69ae5902982839ab2c51",
+    "url": "/coin-chameleon/static/js/main.69ae5902.chunk.js"
+  },
+  {
+    "revision": "ec39fa1e4490d9e6bbe4",
+    "url": "/coin-chameleon/static/css/1.6182c1ec.chunk.css"
+  },
+  {
+    "revision": "ec39fa1e4490d9e6bbe4",
+    "url": "/coin-chameleon/static/js/1.ec39fa1e.chunk.js"
+  },
+  {
+    "revision": "94408476e3ca4680b7c8",
+    "url": "/coin-chameleon/static/js/runtime~main.94408476.js"
+  },
+  {
+    "revision": "54980210316034229c79117065bac998",
+    "url": "/coin-chameleon/static/media/btc.54980210.png"
+  },
+  {
+    "revision": "5c9230e721a0e82bf3c386e9d115c62f",
+    "url": "/coin-chameleon/static/media/link.5c9230e7.png"
+  },
+  {
+    "revision": "a9d19f2c13ca16f6d36ff1f881cd9784",
+    "url": "/coin-chameleon/static/media/ada.a9d19f2c.png"
+  },
+  {
+    "revision": "5febe0133c389d7c3443808a33c0d84c",
+    "url": "/coin-chameleon/static/media/lend.5febe013.png"
+  },
+  {
+    "revision": "b2fcec2408100ade98ba0c5684a0ffdb",
+    "url": "/coin-chameleon/static/media/usdt.b2fcec24.png"
+  },
+  {
+    "revision": "f8728b8ae2f7214af02665b975210b65",
+    "url": "/coin-chameleon/static/media/sol.f8728b8a.png"
+  },
+  {
+    "revision": "502c7d195b27d9415cc034b159c6d002",
+    "url": "/coin-chameleon/index.html"
+  }
+];
